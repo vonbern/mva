26db185f0898873095bf60dbb8eff44c671f6682
Theodoor Bernart <tbernart@outlook.com>
2017-09-26 22:24:09 +0200

    script uitgelijnd

--- Scripting2.ps1
+++ Scripting2.ps1
@@ -4,5 +4,5 @@ param   (
         [string] $drive= 'c:',
         [string] $computer= 'localhost'
         )
-Get-WmiObject -ClassName win32_logicaldisk -Filter "DeviceID='$drive'" -ComputerName "$computer"
+                Get-WmiObject -ClassName win32_logicaldisk -Filter "DeviceID='$drive'" -ComputerName "$computer"
                 }
